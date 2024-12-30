cd28707c2b3d998d1c8e2a8d9e1c64c37217e4ea
Marcin Zientkowski <zientkowskim@gmail.com>
2024-12-30 08:05:21 +0100

    feat(api): companies/import_async

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
+  mount MissionControl::Jobs::Engine, at: "/jobs"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,6 +16,7 @@ Rails.application.routes.draw do
       resources :companies, only: [ :create ] do
         collection do
           post :import
+          post :import_async
         end
       end
     end
